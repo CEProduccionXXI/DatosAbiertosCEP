145749fa2136d687470bde4db457fc261ce760dd
gtrebotic <63752922+gtrebotic@users.noreply.github.com>
2023-01-06 15:36:07 -0300

    Agrega descarga datos

--- data/shiny_app.R
+++ data/shiny_app.R
@@ -31,7 +31,8 @@ ui <- fluidPage(
       tabPanel('Gráfico',
                plotly::plotlyOutput('pride_plot')
     )
-  )
+  ),
+    downloadButton("downloadData", "Descargar datos")
 )
 )
 
@@ -60,6 +61,22 @@ server <- function(input, output) {
       plotly::add_lines(x = x1, y = y1, name = "Red") 
 
   })
+  
+  output$downloadData <- 
+    downloadHandler(
+    filename = function() {
+      paste(input$tabla_dato, '_', input$tabla_universo, "_", input$tabla_provincia, '_', Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      bu <- DatosAbiertosCEP::descarga_DA(tipo=input$tabla_dato,
+                                          jurisdiccion = 'Provincia trabajo',
+                                          sector = 'NO',
+                                          genero = 'NO',
+                                          universo = input$tabla_universo,
+                                          show_info_DA = F) %>%
+        filter(zona_prov == input$tabla_provincia)
+      write.csv(bu, file)
+    })
 }
 
 shinyApp(ui=ui, server=server) 
